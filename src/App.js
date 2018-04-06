4928772af4a755f0e7815162519b81ddf23dfc8f
DesertDemons <H.M.Kamshad@Gmail.com>
2018-04-06 22:08:28 +0300

    Done react 3 starting point 3 with bonus with text msg and alert

--- src/App.js
+++ src/App.js
@@ -7,7 +7,32 @@ import authors from './data';
 import Sidebar from './Sidebar';
 import AuthorsList from './AuthorsList';
 
+import axios from 'axios';
+
 class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      authorsArray: [],
+      loading: true
+    }
+
+}
+componentDidMount() {
+    axios.get('https://the-index-api.herokuapp.com/api/authors/')
+      .then(res => res.data)
+      .then(function (data) {
+        if(data){
+          this.setState({authorsArray: data, loading: false});
+        }
+    })
+      .catch(err => console.error(err));
+  }
+
+  
+
+
+  
   render() {
     return (
       <div id="app" className="container-fluid">
@@ -16,12 +41,19 @@ class App extends Component {
             <Sidebar />
           </div>
           <div className="content col-10">
-            <AuthorsList authors={authors}/>
+            {
+              this.state.loading ? alert('this page is loading authers list please wait ') + '  this page is loading authers list please wait' : <AuthorsList authors={this.state.authors}/>
+            }
+
           </div>
         </div>
       </div>
     );
   }
 }
+
+
+
+
 
 export default App;
